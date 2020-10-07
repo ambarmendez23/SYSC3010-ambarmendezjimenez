ae0d41b40fbae19d48c5d5b19a602e412141ce6c
ambarmendez23 <71477077+ambarmendez23@users.noreply.github.com>
2020-10-06 20:42:29 -0400

    Update and rename Lab/Lab2/lab2-hardware-step1.py to Lab2/lab2-hardware-step1.py

---
+++ Lab2/lab2-hardware-step1.py
@@ -0,0 +1,52 @@
+from sense_hat import SenseHat
+import time
+
+s = SenseHat()
+s.low_light = True
+
+green = (0, 255, 0)
+yellow = (255, 255, 0)
+blue = (0, 0, 255)
+red = (255, 0, 0)
+white = (255,255,255)
+nothing = (0,0,0)
+pink = (255,105, 180)
+
+
+def a():
+    W = white
+    O = nothing
+    logo = [
+    O, O, O, W, W, O, O, O, 
+    O, O, W, O, O, W, W, O,
+    O, W, O, O, O, O, W, O, 
+    O, W, W, W, W, W, W, O,
+    O, W, W, W, W, W, W, O,
+    O, W, O, O, O, O, W, O,
+    O, W, O, O, O, O, W, O,
+    O, O, O, O, O, O, O, O,
+    ]
+    return logo
+def m():
+    W = white
+    O = nothing
+    logo = [
+    O, O, O, O, O, O, O, O, 
+    O, W, O, O, O, O, W, O,
+    O, W, W, O, O, W, W, O, 
+    O, W, O, W, W, O, W, O,
+    O, W, O, O, O, O, W, O,
+    O, W, O, O, O, O, W, O,
+    O, W, O, O, O, O, W, O,
+    O, O, O, O, O, O, O, O,
+    ]
+    return logo
+
+
+images = [a,m] 
+count = 0
+
+while True: 
+    s.set_pixels(images[count % len(images)]())
+    time.sleep(.75)
+    count += 1
